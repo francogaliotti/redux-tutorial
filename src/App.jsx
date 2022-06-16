ae9ea88ae943f8c19271f2c87f5a1e97f341d629
francogaliotti <francogaliotti@gmail.com>
2022-06-15 23:59:12 -0300

    bootstrap library test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
 import { addUser, deleteUser, updateUsername } from './features/users';
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 function App() {
@@ -14,19 +15,20 @@ function App() {
 
   return (
     <div className="App">
-      <div className="addUser">
-        <input type="text" onChange={(e) => setName(e.target.value)} placeholder='Name...' />
-        <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder='Username...' />
-        <button onClick={() => { dispatch(addUser({ id: userList[userList.length - 1].id + 1, name, username })) }}>Add User</button>
+      <div className="addUser mt-4 py-3 px-5 rounded shadow-lg">
+        <h2>Create User</h2>
+        <input className="form-control mt-2" type="text" onChange={(e) => setName(e.target.value)} placeholder='Name...' />
+        <input className="form-control mt-2" type="text" onChange={(e) => setUsername(e.target.value)} placeholder='Username...' />
+        <button className="btn btn-success mt-2" style={{width: '100%'}} onClick={() => { dispatch(addUser({ id: userList[userList.length - 1].id + 1, name, username })) }}>Add User</button>
       </div>
       <div className="displayUsers">
         {userList.map(user => {
           return <div>
             <h1>{user.name}</h1>
             <h2>{user.username} id:{user.id}</h2>
-            <input type="text" placeholder='NewUsername'onChange={(e) => setNewUsername(e.target.value)}/>
-            <button onClick={()=>{dispatch(updateUsername({id: user.id, username: newUsername}))}}> Update Username</button>
-            <button onClick={()=>{dispatch(deleteUser({id:user.id}))}}> Delete User</button>
+            <input className="form-control mb-2" style={{width: '60%', marginLeft: '20%'}} type="text" placeholder='NewUsername'onChange={(e) => setNewUsername(e.target.value)}/>
+            <button className='btn btn-primary me-2 shadow' onClick={()=>{dispatch(updateUsername({id: user.id, username: newUsername}))}}> Update Username</button>
+            <button className='btn btn-danger ms-2 shadow' onClick={()=>{dispatch(deleteUser({id:user.id}))}}> Delete User</button>
           </div>
         })}
       </div>
